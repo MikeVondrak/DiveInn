f0d1bdcea402cf8ad2e596621ef12349b7faeec8
m v <mvondrak@gmail.com>
2017-04-21 06:06:26 -0600

    remove Angular Seed files

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>Coming soon... {{name}}!</h1>`,
-})
-export class AppComponent  { name = 'The Dive Inn'; }
