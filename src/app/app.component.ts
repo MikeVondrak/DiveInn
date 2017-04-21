2cd98691387e0e6217eeed1b643f30587e1e2fb5
m v <mvondrak@gmail.com>
2017-04-21 05:18:30 -0600

    Initial setup using Angular Seed project https://github.com/angular/quickstart

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Coming soon... {{name}}!</h1>`,
+})
+export class AppComponent  { name = 'The Dive Inn'; }
